fea93edf2dcb0a32ed3aa2237811d432ab9b9c1b
Bassist_Zero <bassist.zero@icloud.com>
2023-02-04 22:53:45 +0300

    Add description cell

---
+++ Surf_Test_Project/MainFlow/MainModule/View/Cells/DescriptionTableViewCell/DescriptionTableViewCell.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+final class DescriptionTableViewCell: UITableViewCell {
+
+    // MARK: - Private Outlets
+
+    @IBOutlet private weak var tableView: UITableView!
+
+    // MARK: - UITableViewCell
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        configureAppearance()
+    }
+
+}
+
+// MARK: - Configuration
+
+private extension DescriptionTableViewCell {
+
+    func configureAppearance() {
+        configureTableView()
+    }
+
+    func configureTableView() {
+        tableView.separatorStyle = .none
+
+        tableView.showsVerticalScrollIndicator = false
+        tableView.showsHorizontalScrollIndicator = false
+
+        tableView.isScrollEnabled = false
+    }
+
+}
